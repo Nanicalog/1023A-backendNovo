9c8d960f22b0dfbedbe4b9c041da4e5f377132ef
Nanica.log <farlany.domingues@estudante.ifms.edu.br>
2025-08-19 09:02:51 -0300

    #Primeiro commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-import 'dotenv/config'
-console.log(process.env);
-//console.log("oi")
+import 'dotenv/config';
+import express from 'express';
+
+console.log(process.env.DBUSER);  
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`DBUSER: ${process.env.DBUSER}`);
+});
+
+app.listen(8000, () => {
+    console.log('Servidor rodando na porta 8000 moh!');
+});
